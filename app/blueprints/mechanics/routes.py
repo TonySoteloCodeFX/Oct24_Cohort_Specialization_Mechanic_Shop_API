a364df5610026dc0203bfbb4280f9c39c6fe354c
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-04-12 20:20:19 -0700

    Added Mechanics route

--- app/blueprints/mechanics/routes.py
+++ app/blueprints/mechanics/routes.py
@@ -5,3 +5,60 @@ from . import mechanics_bp
 from .schemas import mechanic_schema, mechanics_schema
 from app.models import Mechanic, db
 # -------------------------------------------------------------------------------> Create Mechanic Route
+@mechanics_bp.route('/', methods=['POST'])
+def create_mechanic():
+    try:
+        mechanic_data = mechanic_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    query = select(Mechanic).where(Mechanic.email == mechanic_data['email'])
+    mechanic = db.session.execute(query).scalars().first()
+
+    if not mechanic:
+        new_mechanic = Mechanic(**mechanic_data)
+        db.session.add(new_mechanic)
+        db.session.commit()
+        return mechanic_schema.jsonify(new_mechanic), 201
+    return jsonify({"error": "Email already exists."}), 400
+# -------------------------------------------------------------------------------> Get All Mechanics Route
+@mechanics_bp.route('/', methods=['GET'])
+def get_mechanics():
+    query = select(Mechanic)
+    mechanics = db.session.execute(query).scalars().all()
+    return mechanics_schema.jsonify(mechanics), 200
+# -------------------------------------------------------------------------------> Get Mechanics By ID Route
+@mechanics_bp.route('/<int:mechanic_id>', methods=['GET'])
+def get_mechanic_id(mechanic_id):
+    mechanic = db.session.get(Mechanic, mechanic_id)
+
+    if mechanic:
+        return mechanic_schema.jsonify(mechanic), 200
+    return jsonify({"error": "Mechanic does not exist."})
+# -------------------------------------------------------------------------------> Update Mechanic Route
+@mechanics_bp.route('/<int:mechanic_id>', methods=['PUT'])
+def update_mechanic(mechanic_id):
+    try:
+        mechanic_data = mechanic_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    mechanic = db.session.get(Mechanic, mechanic_id)
+
+    if mechanic:
+        for field, value in mechanic_data.items():
+            setattr(mechanic, field, value)
+        db.session.commit()
+        return mechanic_schema.jsonify(mechanic)
+    return jsonify({"error:" "Customer ID does not exist."})
+# -------------------------------------------------------------------------------> Delete Mechanic Route
+@mechanics_bp.route('/<int:mechanic_id>', methods=['DELETE'])
+def delete_mechanic(mechanic_id):
+
+    mechanic = db.session.get(Mechanic, mechanic_id)
+
+    if mechanic:
+        db.session.delete(mechanic)
+        db.session.commit()
+        return jsonify(f"Deleted Mechanic: {mechanic.name}"), 200
+    return jsonify({"error": "Customer does not exist."})
