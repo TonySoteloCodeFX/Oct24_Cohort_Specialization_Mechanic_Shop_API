cdee1b959e74895ff1b53da8c837ae10d16a741c
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-04-16 01:07:51 -0700

    Get All Tickets Route Done

--- app/blueprints/tickets/routes.py
+++ app/blueprints/tickets/routes.py
@@ -3,7 +3,7 @@ from marshmallow import ValidationError
 from sqlalchemy import select
 from . import tickets_bp
 from .schemas import ticket_schema, tickets_schema
-from app.models import Ticket, db, Mechanic
+from app.models import Ticket, db, Mechanic, Service
 # -------------------------------------------------------------------------------> Create Ticket Route
 @tickets_bp.route('/', methods=['POST'])
 def create_ticket():
@@ -15,17 +15,31 @@ def create_ticket():
     
     new_ticket = Ticket(service_date=ticket_data['service_date'], vin=ticket_data['vin'], customer_id=ticket_data['customer_id'])
 
-
     for mechanic_id in ticket_data["mechanic_ids"]:
         query = select(Mechanic).where(Mechanic.id==mechanic_id)
         mechanic = db.session.execute(query).scalar()
         if mechanic:
             new_ticket.mechanics.append(mechanic)
         else:
-            return jsonify({"Message": "Invalid mechanic ID."})
+            return jsonify({"Message": "Invalid mechanic ID."}), 400
+    
+    for service_id in ticket_data["service_ids"]:
+        query = select(Service).where(Service.id==service_id)
+        service = db.session.execute(query).scalar()
+        if service:
+            new_ticket.services.append(service)
+        else:
+            return jsonify({"Message": "Invalid service ID."}), 400
+
     
     db.session.add(new_ticket)
     db.session.commit()
 
     return ticket_schema.jsonify(new_ticket), 201
-
+# -------------------------------------------------------------------------------> Get All Tickets Route
+@tickets_bp.route('/', methods=['GET'])
+def get_tickets():
+    query = select(Ticket)
+    tickets = db.session.execute(query).scalars().all()
+    return tickets_schema.jsonify(tickets), 200
+# -------------------------------------------------------------------------------> Get Ticket by ID NEXT UP
--- app/blueprints/tickets/schemas.py
+++ app/blueprints/tickets/schemas.py
@@ -1,14 +1,36 @@
 from app.extensions import ma
-from app.models import Ticket
-from typing import List
-# -------------------------------------------------------------------------------> Schema Ticket
+from app.models import Ticket, Mechanic, Customer, Service
+from marshmallow import fields
+
+# -------------------------------------------------------------------------------> Mechanic Schema 
+class MechanicNestedSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Mechanic
+        fields = ('id', 'name')
+# -------------------------------------------------------------------------------> Service Schema
+class ServiceNestedSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Service
+        fields = ('id', 'service_desc',)
+# -------------------------------------------------------------------------------> Customer Schema
+class CustomerNestedSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer
+        fields = ('id', 'name')
+# -------------------------------------------------------------------------------> Ticket Schema
 class TicketSchema(ma.SQLAlchemyAutoSchema):
     mechanic_ids = ma.List(ma.Integer(), load_only=True)
+    mechanics = ma.Nested(MechanicNestedSchema, many=True, dump_only=True)
+
+    service_ids = ma.List(ma.Integer(), load_only=True)
+    services = ma.Nested(ServiceNestedSchema, many=True, dump_only=True)
+
+    customer = ma.Nested(CustomerNestedSchema, dump_only=True)
+    customer_id = fields.Integer(load_only=True)
 
     class Meta:
         model = Ticket
-        fields = ('service_date', 'vin', 'customer_id', 'mechanic_ids')
-
+        fields = ('id', 'service_date', 'vin', 'customer', 'customer_id', 'mechanic_ids', 'mechanics', 'service_ids', 'services')
 
 ticket_schema = TicketSchema()
 tickets_schema = TicketSchema(many=True)
