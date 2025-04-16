7f42d17f233d1f9d1eeef17243f81db43f436c82
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-04-15 23:56:20 -0700

    Create Ticket Route Added

--- app/blueprints/tickets/routes.py
+++ app/blueprints/tickets/routes.py
@@ -3,7 +3,7 @@ from marshmallow import ValidationError
 from sqlalchemy import select
 from . import tickets_bp
 from .schemas import ticket_schema, tickets_schema
-from app.models import Ticket, db
+from app.models import Ticket, db, Mechanic
 # -------------------------------------------------------------------------------> Create Ticket Route
 @tickets_bp.route('/', methods=['POST'])
 def create_ticket():
@@ -12,4 +12,20 @@ def create_ticket():
         print(ticket_data)
     except ValidationError as e:
         return jsonify(e.messages), 400
-    return jsonify({"Message": "Nice Work"})
+    
+    new_ticket = Ticket(service_date=ticket_data['service_date'], vin=ticket_data['vin'], customer_id=ticket_data['customer_id'])
+
+
+    for mechanic_id in ticket_data["mechanic_ids"]:
+        query = select(Mechanic).where(Mechanic.id==mechanic_id)
+        mechanic = db.session.execute(query).scalar()
+        if mechanic:
+            new_ticket.mechanics.append(mechanic)
+        else:
+            return jsonify({"Message": "Invalid mechanic ID."})
+    
+    db.session.add(new_ticket)
+    db.session.commit()
+
+    return ticket_schema.jsonify(new_ticket), 201
+
--- app/blueprints/tickets/schemas.py
+++ app/blueprints/tickets/schemas.py
@@ -1,10 +1,14 @@
 from app.extensions import ma
 from app.models import Ticket
+from typing import List
 # -------------------------------------------------------------------------------> Schema Ticket
 class TicketSchema(ma.SQLAlchemyAutoSchema):
+    mechanic_ids = ma.List(ma.Integer(), load_only=True)
+
     class Meta:
         model = Ticket
-        fields= ('mechanic_id', 'service_date', 'vin', 'customer_id')
+        fields = ('service_date', 'vin', 'customer_id', 'mechanic_ids')
+
 
 ticket_schema = TicketSchema()
 tickets_schema = TicketSchema(many=True)
