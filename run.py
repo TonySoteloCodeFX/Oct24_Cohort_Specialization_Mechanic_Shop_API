f6aaa93eae2a57953125f45c7cd8c3c879c7addb
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-05-08 22:19:16 -0700

    updated

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 from app. models import db
 from flask_cors import CORS
+import os
 
 
 
@@ -11,6 +12,9 @@ with app.app_context():
     db.create_all()
 
 CORS(app)
-    
-app.run()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
+
 
