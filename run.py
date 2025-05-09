3dca3b4f79fec7f81adc5e950a862dcd553681a7
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-05-08 21:44:17 -0700

    Prepped for Deploy

---
+++ run.py
@@ -0,0 +1,16 @@
+from app import create_app
+from app. models import db
+from flask_cors import CORS
+
+
+
+app = create_app('ProductionConfig')
+
+with app.app_context():
+    # db.drop_all()
+    db.create_all()
+
+CORS(app)
+    
+app.run()
+
