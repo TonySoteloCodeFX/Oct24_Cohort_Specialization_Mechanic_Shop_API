eefa90864c1427d4f3b9ee95ee5b3685cbe8aa8d
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-05-08 22:39:34 -0700

    Updated

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from app import create_app
 from app. models import db
 from flask_cors import CORS
+from flask import redirect
 import os
 
 
 
 app = create_app('ProductionConfig')
+
+@app.route('/', methods = ['GET'])
+def index():
+    return redirect('/api/docs')
 
 with app.app_context():
     # db.drop_all()
@@ -14,7 +19,7 @@ with app.app_context():
 CORS(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='tonys-my-mechanic-shop-api.onrender.com', port=int(os.environ.get("PORT", 5000)))
 
 
 
